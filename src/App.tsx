d38d0b3fbd8c1d8e2442dca03c4baf9a0186c1bf
qkr132546 <qkr132546@naver.com>
2025-06-26 16:51:17 +0900

    project setting

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { ThemeProvider } from "./components/ui/theme-provider";
+
+function App() {
+  return (
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button variant={"gradient"}>Click me</Button>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
