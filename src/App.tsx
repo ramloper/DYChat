9b3161a134efb8c1f90a7dd4ca56d0e3acd9ecad
ramloper <rla7360@gmail.com>
2025-06-26 17:12:34 +0900

    fix : prettierrc

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,13 @@ import { Button } from "@/components/ui/button";
 import { ThemeProvider } from "./components/ui/theme-provider";
 
 function App() {
-  return (
-    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button variant={"gradient"}>Click me</Button>
-      </div>
-
-
-
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+            <div className="flex min-h-svh flex-col items-center justify-center">
+                <Button variant={"gradient"}>Click me</Button>
+            </div>
+        </ThemeProvider>
+    );
 }
 
 export default App;
